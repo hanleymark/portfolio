9c3f02580c5b493c7212022d7384d46248e2141b
Mark hanley <home@markhanley.co.uk>
2022-10-09 22:20:47 +0100

    Added collapse all text function

--- portfolio-script.js
+++ portfolio-script.js
@@ -1,12 +1,25 @@
-function showHideCardText(textID, textIntroID) {
-    var text = document.getElementById(textID);
-    var textIntro = document.getElementById(textIntroID);
+function showHideCardText(textID, textIntroID, buttonID) {
+    var text = document.querySelector(textID);
+    var textIntro = document.querySelector(textIntroID);
+    var button = document.querySelector(buttonID);
 
     if (text.style.display === "none") {
       text.style.display = "block";
+      button.innerHTML="Show less";
     } else {
       text.style.display = "none";
+      button.innerHTML="Show more";
     }
     // Make sure first words of card text are displayed
     textIntro.style.display = "block";
   }
+
+  function collapseAllCardText() {
+    var allCardTexts = document.querySelectorAll(".card-text");
+
+    allCardTexts.forEach(element => {
+        element.style.display = "none";
+    });
+  }
+
+  collapseAllCardText();
